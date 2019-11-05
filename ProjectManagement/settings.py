242f378fd4a7bfc5a2ae5482d664e1c21be15d14
Ubuntu <ubuntu@ip-172-31-41-112.ec2.internal>
2019-11-05 09:15:07 +0000

    edit settings for deployement

--- ProjectManagement/settings.py
+++ ProjectManagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,6 +25,8 @@ SECRET_KEY = 'g9q2*hne835-0r_3gqf8%fwj!6nksrvu1a3vpl#@44axw+e_^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# ALLOWED_HOSTS = ["dashboard.heroku.com/apps/nd-project-management"]
 
 ALLOWED_HOSTS = ["*"]
 
@@ -78,13 +81,15 @@ WSGI_APPLICATION = 'ProjectManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+
+DATABASES = {'default': dj_database_url.parse(os.environ['DATABASE_URL'])}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
